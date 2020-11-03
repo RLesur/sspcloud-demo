056c1c5ae74cf29451bac9772515b00985f11c7c
Romain Lesur <romain.lesur@insee.fr>
2020-11-04 00:06:01 +0100

    add build script

---
+++ _upload.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+# Ceci est un exemple pour uploader tous les fichiers HTML présents dans le répertoire courant
+bucket <- "f7sggu"
+files <- list.files(pattern = "\\.htm[l]$")
+
+for(f in files) {
+  cat("Uploading", f, "file...\n")
+  aws.s3::put_object(f, file.path(basename(getwd()), f), bucket, region = "", show_progress = TRUE)
+}
